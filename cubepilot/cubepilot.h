7b7107baf5be7f52069bc0e7bca056dff4f971d5
Gangik Cho <gicho@airbility.co.kr>
2025-05-21 13:16:43 +0900

    autogenerated headers for 169c64ba8ffbb0f6ee91cbb5bc9fcf622a4cb4e4

--- cubepilot/cubepilot.h
+++ cubepilot/cubepilot.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_CUBEPILOT.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_CUBEPILOT_XML_HASH 618386182864917645
+#define MAVLINK_CUBEPILOT_XML_HASH -5825373133885089718
 
 #ifdef __cplusplus
 extern "C" {
--- tilt_ctrl/tilt_ctrl.h
+++ tilt_ctrl/tilt_ctrl.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_TILT_CTRL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_TILT_CTRL_XML_HASH -7276094771961915554
+#define MAVLINK_TILT_CTRL_XML_HASH 3124328164799449765
 
 #ifdef __cplusplus
 extern "C" {
