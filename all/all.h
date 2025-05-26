ead0cac277d699dc871b6b3e8bdf0cffba93ba45
Gangik Cho <gicho@airbility.co.kr>
2025-05-26 13:50:04 +0900

    autogenerated headers for a6cd0af80eadf6f269ea260aae196042a837a515

--- all/all.h
+++ all/all.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ALL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ALL_XML_HASH -7716857215127244958
+#define MAVLINK_ALL_XML_HASH 66461047458976090
 
 #ifdef __cplusplus
 extern "C" {
--- tilt_ctrl/tilt_ctrl.h
+++ tilt_ctrl/tilt_ctrl.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_TILT_CTRL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_TILT_CTRL_XML_HASH -2033948045716858683
+#define MAVLINK_TILT_CTRL_XML_HASH -2265093150196906307
 
 #ifdef __cplusplus
 extern "C" {
