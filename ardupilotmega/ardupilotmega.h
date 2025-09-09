f1386175cb6367c6b9e917916686a636ec58cda8
SangminLee <sangmin.lee@airbility.co.kr>
2025-09-09 16:51:42 +0900

    autogenerated headers for a5561126878c8a94ee28f70c4a2c0a02bc28a446

--- ardupilotmega/ardupilotmega.h
+++ ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH -7275619721916467902
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH 5281537059770105027
 
 #ifdef __cplusplus
 extern "C" {
