ae3a476ae7397696ddf4d3a19f35a9ab43e77c8f
SangminLee <sangmin.lee@airbility.co.kr>
2025-09-10 09:56:44 +0900

    autogenerated headers for d61295924d746dae14b49c415055a78da6d1f386

--- AVSSUAS/AVSSUAS.h
+++ AVSSUAS/AVSSUAS.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_AVSSUAS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_AVSSUAS_XML_HASH -7108134792018594738
+#define MAVLINK_AVSSUAS_XML_HASH 2360952275858604840
 
 #ifdef __cplusplus
 extern "C" {
--- loweheiser/loweheiser.h
+++ loweheiser/loweheiser.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_LOWEHEISER.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_LOWEHEISER_XML_HASH 82422513886486598
+#define MAVLINK_LOWEHEISER_XML_HASH 8342434921208413022
 
 #ifdef __cplusplus
 extern "C" {
